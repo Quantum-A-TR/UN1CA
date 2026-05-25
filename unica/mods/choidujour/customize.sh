31c76419044c097ad9e1e953cd368d2c6492e429
Quantum-A-TR <emirefedikmen36@gmail.com>
2026-05-25 21:32:58 +0300

    Revert "gen_config_file: use my certs"
    This reverts commit e3221a58d81596e7e770c4434bb4007a13a8db0a.

--- unica/mods/choidujour/customize.sh
+++ unica/mods/choidujour/customize.sh
@@ -17,12 +17,9 @@ fi
 
 ADD_TO_WORK_DIR "$MODPATH" "system" "." 0 0 755 "u:object_r:system_file:s0"
 
-DECODE_APK "system" "system/priv-app/ChoiDujour/ChoiDujour.apk"
-
 LOG "- Patching /system/system/etc/security/otacerts.zip"
 EVAL "rm \"$WORK_DIR/system/system/etc/security/otacerts.zip\""
 EVAL "cd \"$SRC_DIR\"; zip -q \"$WORK_DIR/system/system/etc/security/otacerts.zip\" \"./security/unica_ota.x509.pem\""
-EVAL "cd \"$MODPATH\"; zip -q \"$WORK_DIR/system/system/etc/security/otacerts.zip\" \"./security/unica_ota_official.x509.pem\""
 
 DECODE_APK "system" "system/priv-app/SecSettings/SecSettings.apk"
 
